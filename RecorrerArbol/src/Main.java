3abba4e3e0d40f66cd4a271f9a49eee8e8baeeaa
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-05-14 09:48:51 -0500

    Marca error

--- RecorrerArbol/src/Main.java
+++ RecorrerArbol/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
     
     public static void main(String[] args) {
+
+       NodoArbol<Integer> tree = new NodoArbol<>();
+
+       tree.setDato(20);
        
+       tree.setDato(new NodoArbol[]{new NodoArbol(23, null), new NodoArbol(11, null)});
     }
-    
+
 }
--- RecorrerArbol/src/NodoArbol.java
+++ RecorrerArbol/src/NodoArbol.java
@@ -1,10 +1,28 @@
+public class NodoArbol<T> {
 
-    public class NodoArbol<T> {
     private T dato;
     private NodoArbol[] hijo;
 
     public NodoArbol(T dato, NodoArbol[] hijo) {
         this.dato = dato;
+        this.hijo = hijo;
+    }
+    NodoArbol() {
+    }
+    
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public NodoArbol[] getHijo() {
+        return hijo;
+    }
+
+    public void setHijo(NodoArbol[] hijo) {
         this.hijo = hijo;
     }
     
