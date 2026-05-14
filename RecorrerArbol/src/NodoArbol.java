94a832701d552a26bb1038fa240513ae56046ef6
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-05-14 09:04:02 -0500

    Se creo el archivo RecorrerArbol con una clase NodoArbol

--- RecorrerArbol/src/NodoArbol.java
+++ RecorrerArbol/src/NodoArbol.java
@@ -1,4 +1,12 @@
 
-public class NodoArbol {
+    public class NodoArbol<T> {
+    private T dato;
+    private NodoArbol[] hijo;
+
+    public NodoArbol(T dato, NodoArbol[] hijo) {
+        this.dato = dato;
+        this.hijo = hijo;
+    }
     
 }
+
